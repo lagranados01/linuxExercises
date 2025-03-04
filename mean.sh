f257e11bb818f82bc64105e08b5a1b147acb6504
Jackson Wegner <jswegnerwisc.edu>
2025-03-04 09:40:00 -0600

    finalized version, all commands working

--- mean.sh
+++ mean.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]; then # Recall: "-ne" checks integer inequality.
-echo "usage: $0" 1>&2 # write error message to stderr (below)
-exit 0
+#Usage Statement
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    echo "usage: $0 <column number>, [file name]" 1>&2
+    exit 1
 fi
+
+#Grab Input
+column=$1
+file=$2
+
+if [[ -z "$file" ]]; then
+    input_stream="/dev/stdin"
+else
+    input_stream="$file"
+fi
+
+#Loop for the mean
+cut -d, -f$column "$input_stream" | tail -n +2 | {
+    sum=0
+    count=0
+
+    while read -r value; do
+        sum=$(echo "$sum + $value" | bc)
+        count=$((count + 1))
+    done
+
+    mean=$(echo "scale=2; $sum / $count" | bc)
+    echo "Mean: $mean"
+}
